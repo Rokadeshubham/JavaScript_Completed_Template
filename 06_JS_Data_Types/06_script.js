15ff6a3e83a26a727858652165836f779ba3482d
Shubham Rokade <shubhamrokade211@gmail.com>
2018-08-01 23:52:51 +0530

    Add files via upload

--- 06_JS_Data_Types/06_script.js
+++ 06_JS_Data_Types/06_script.js
@@ -1,44 +1,36 @@
 // number example
-var jsVersion= 5;
-console.log("The JS version is:"+jsVersion + "  Data Tyape " +typeof jsVersion)
-
+var jsVersion = 5;
+console.log("value : " + jsVersion + " Data Type : " + typeof jsVersion);
 
 // String Examples
-var studentName = "John";
-console.log("name:"+studentName +"  Data type:  " +typeof studentName)
-
+var studentName = 'Rajan';
+console.log("value : " + studentName + " Data Type : " + typeof studentName);
 
 // boolean Examples
-var isJSEasy =true;
-console.log("value:" + isJSEasy + "data type: " +typeof isJSEasy)
+var isJSEasy = true;
+console.log("value : " + isJSEasy + " Data Type : " + typeof isJSEasy);
 
 // Undefined Example
-var abc;//just declared
-console.log("value:"+abc + " Data Type: " +typeof abc)
-
-
-
+var abc; // just declared
+console.log("value : " + abc + " Data Type : " + typeof abc);
 
 // null example
-
 var xyz = null;
-console.log("value+" +xyz+ " Data Type: " +typeof xyz)
-
-
+console.log("value : " + xyz + " Data Type : " + typeof xyz);
 
 // reassignment example of variables
 var test;
-console.log("test:" +test);//undefined
+console.log('test: ' + test); // undefined
 
-test=10;
-console.log("test:" +test);//10
+test = 10;
+console.log('test: ' + test); // 10
 
-test='JS';
-console.log("test:" +test);//JS
+test = 'JS';
+console.log('test: ' + test); // JS
 
-test=true;
-console.log("test:" +test);//true
+test = true;
+console.log('test: ' + test); // true
 
-test=null;
-console.log("test:" +test);//null
+test = null;
+console.log('test: ' + test); // null
 
--- 07_JS_Operators/07_script.js
+++ 07_JS_Operators/07_script.js
@@ -1,85 +1,75 @@
 // Assignment operator =
 var num1 = 10;
-console.log("value of a:"+num1);
+console.log("num1 : " + num1);
 
 // Arithmetic operators + - * /
 var a = 10;
-var b = 30;
+var b = 20;
 var sum = a + b;
-console.log("The Sum Of a and b is:" +sum);
-
-
+console.log("Sum of a , b is : " + sum);
 
 // Short hand math += , -= , *= , /=
-
 var p = 10;
 var q = 20;
 var add = 0;
-add = add + (p+q);
-add += (p+q);
-console.log(add);
+add = add + (p + q); // 0 + 10 + 20 = 30
+add += (p + q); // 30 + 10 + 20 = 60
+console.log(add); // 60
 
 // Conditional operators < , > , <= , >= , !=
 var age = 25;
-if(age< 18){
-    console.log("You are minor");
+if(age < 18){
+    console.log('You are Minor');
 }
 else{
     console.log('You are Major');
 }
 
-
 // Unary Operator ++ , -- (pre , post)
 var x = 10;
-x = x +1;
-x += 1;
-x++;
-console.log(x);
+x = x + 1; // 11
+x += 1; // 12
+x++; // 13
+console.log(x); // 13
 
 // Logical operators AND , OR
 var inRelation = true;
-var parentAgreed = false;
-if(inRelation && parentAgreed){
-    console.log('get mary soon');
+var parentsAgreed = false;
+if(inRelation && parentsAgreed){
+    console.log('Get Marry Soon');
 }
 else{
-    console.log('wait until parents agreed');
+    console.log('Wait Until the parents Agree');
 }
 
-// String Concatenation Operator(+)
-var test = 10 + 20 ;
-console.log(test);
-var test = 10 + 30 + "20" +10 +20 +50 ;
-console.log(test);
-
-
+// String Concatenation Operator (+)
+var test = 1 + 2 + "2" + 1 + 3 + "2";
+console.log(test); // 32132
 
 // Ternary operator (? :)
 age = 25;
-(age < 18) ? console.log('Your Minor'): console.log('Your Major');
+(age < 18) ? console.log('You Are Minor') : console.log('You Are Major');
 
-// Type of operator
+// Typeof operator
 var abc = 10;
-console.log('value:' +abc + " DataType " +typeof abc)
-
+console.log('value : ' + abc + " Data Type : " + typeof abc);
 
 // == operator
-var a =10;
-var b = 10;
-if(a ==  b){
-    console.log('Both are equal');
+a = 10;
+b = "10";
+if(a == b){
+    console.log("Both are Equal");
 }
 else{
-    console.log('Both are not equal');
+    console.log("Both are NOT Equal");
 }
-
 
 // === operator
-var a =10;
-var b = "100";
-if(a ===  b){
-    console.log('Both are equal');
+a = 10;
+b = "10";
+if(a === b){
+    console.log("Both are Equal");
 }
 else{
-    console.log('Both are not equal');
+    console.log("Both are NOT Equal");
 }
